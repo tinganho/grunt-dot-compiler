0f848d25017c7e3908b58940c0287695b1962381
Markus Ullmark <ullmark@gmail.com>
2012-11-26 22:18:07 +0100

    fixed expectation

--- test/expected/foo.js
+++ test/expected/foo.js
@@ -1,5 +1,5 @@
 (function compileHoganTemplates() {
-  foo.tmpl=window.tmpl||{};
+  foo.tmpl=foo.tmpl||{};
   foo.tmpl.bar=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
   foo.tmpl.foo=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
 }());
