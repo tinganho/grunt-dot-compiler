4c8ac57bea494d67c1527cc3f25309782c5893a8
Alan Shaw <alan@freestyle-developments.co.uk>
2012-12-11 10:51:19 +0000

    Fix up the tests

--- test/expected/bar.js
+++ test/expected/bar.js
@@ -1,5 +1,5 @@
 (function compileTemplates() {
-  window.tmpl=window.tmpl||{};
-  window.tmpl.bar=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-  window.tmpl.foo=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
+	window.tmpl=window.tmpl||{};
+	tmpl['bar']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
+	tmpl['foo']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
 }());
--- test/expected/foo.js
+++ test/expected/foo.js
@@ -1,5 +1,6 @@
 (function compileTemplates() {
-  foo.tmpl=foo.tmpl||{};
-  foo.tmpl.bar=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-  foo.tmpl.foo=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
+	window.foo=window.foo||{};
+	window.foo.tmpl=window.foo.tmpl||{};
+	foo.tmpl['bar']=new Ext.XTemplate('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
+	foo.tmpl['foo']=new Ext.XTemplate('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
 }());
