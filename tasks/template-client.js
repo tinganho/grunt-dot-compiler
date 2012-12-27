96afc6592f1782b644d7f05df4c9adf1f5bb5da9
Tingan Ho <tingan@clockies.com>
2012-12-27 22:58:59 +0800

    Refactored the dot template compiler

--- tasks/template-client.js
+++ tasks/template-client.js
@@ -11,12 +11,12 @@ module.exports = function(grunt) {
 	"use strict";
 
 	grunt.util = grunt.util || grunt.utils;
-	
+
 	var _ = grunt.utils._;
 
 	var path = require('path'),
 		fs = require('fs'),
-		cleaner = /^\s+|\s+$|[\r?\n]+/gm;
+		cleaner = /^\s+|\s+$|[\r\n]+/gm;
 
 	// Please see the grunt documentation for more information regarding task and
 	// helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
@@ -25,11 +25,11 @@ module.exports = function(grunt) {
 	// TASKS
 	// ==========================================================================
 
-	grunt.registerMultiTask('templateclient', 'prepares and combines any type of template into a script include', function() {
+	grunt.registerMultiTask('template-client', 'prepares and combines any type of template into a script include', function() {
 		// grap the filepattern
 		var files = grunt.file.expandFiles(this.file.src);
 		// create the hogan include
-		var src = grunt.helper('templateclient', files, this.data.options);
+		var src = grunt.helper('template-client', files, this.data.options);
 		// write the new file
 		grunt.file.write(this.file.dest, src);
 		// log our success
@@ -40,7 +40,7 @@ module.exports = function(grunt) {
 	// HELPERS
 	// ==========================================================================
 
-	grunt.registerHelper('templateclient', function(files, options) {
+	grunt.registerHelper('template-client', function(files, options) {
 		var js = '';
 
 		options = _.defaults(options || {}, {
@@ -48,31 +48,51 @@ module.exports = function(grunt) {
 			key: function(filepath) {
 				return path.basename(filepath, path.extname(filepath));
 			},
-			prefix: 'Hogan.compile(',
-			suffix: ')'
+			prefix: 'doT.template(',
+			suffix: ')',
+			nodeCompile: true
 		});
-		
+
 		options.variable = options.variable.replace('window.', '');
 
-		js += '(function compileTemplates() {' + grunt.utils.linefeed;
-		
-		var currentVar = 'window';
+    js += 'if( typeof define !== "function" ) {' + grunt.utils.linefeed;
+    js +=   'define = require( "amdefine" )( module )' + grunt.utils.linefeed;
+    js += '}' + grunt.utils.linefeed;
+
+		js += 'define(function() {' + grunt.utils.linefeed;
+
 		var variables = options.variable.split('.');
-		
+
 		_.each(variables, function(v) {
-			currentVar = currentVar + '.' + v;
-			js += '	' + currentVar + '=' + currentVar + '||{};' + grunt.utils.linefeed;
-		});
-		
-		files.map(function(filepath) {
-			
-			var key = options.key(filepath);
-			var contents = grunt.file.read(filepath).replace(cleaner, '').replace(/'/g, "\\'");
-			
-			js += '	' + options.variable + "['" + key + "']=" + options.prefix + '\'' + contents + '\'' + options.suffix + ';' + grunt.utils.linefeed;
+			js += 'var ' + v + '=' + v + '|| {};' + grunt.utils.linefeed;
 		});
 
-		js += '}());' + grunt.utils.linefeed;
+		var doT = require( 'dot' );
+
+    var defs = {};
+    defs.loadfile = function( path ) {
+      return fs.readFileSync( path );
+    };
+    defs.root = options.root;
+
+		files.map(function(filepath) {
+
+			var key = options.key(filepath);
+			var contents = grunt.file.read(filepath).replace(cleaner, '').replace(/'/g, "\\'");
+
+			var compile = options.prefix + '\'' + contents + '\', undefined, defs' + options.suffix + ';' + grunt.utils.linefeed;
+
+			if( options.nodeCompile ) {
+				compile = eval( compile );
+				console.log(key + ' = ' + compile);
+			}
+
+			js += '	' + options.variable + "['" + key + "']=" + compile + grunt.utils.linefeed;
+
+
+		});
+
+		js += 'return ' + options.variable + ';});' + grunt.utils.linefeed;
 
 		return js;
 	});
