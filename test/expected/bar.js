ea1df529be7e335936a230696ee9f2ca3bcbbeb2
Tingan Ho <tingan@clockies.com>
2012-12-31 16:01:13 +0800

    Deleted som unused files

--- test/expected/bar.js
+++
@@ -1,5 +0,0 @@
-(function compileTemplates() {
-	window.tmpl=window.tmpl||{};
-	tmpl['bar']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-	tmpl['foo']=Hogan.compile('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-}());
--- test/expected/foo.js
+++
@@ -1,6 +0,0 @@
-(function compileTemplates() {
-	window.foo=window.foo||{};
-	window.foo.tmpl=window.foo.tmpl||{};
-	foo.tmpl['bar']=new Ext.XTemplate('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-	foo.tmpl['foo']=new Ext.XTemplate('<div class="something"><ul>{{#foo}}<li><h1>{{bar}}</h1></li>{{/foo}}</ul></div>');
-}());
