92a2ca2ddd7a45415d7327fd28de4a25715214a1
Tingan Ho <tingan87@gmail.com>
2013-03-12 16:05:50 +0800

    Removed some files

--- test/expected/tmpl.js
+++
@@ -1,9 +0,0 @@
-if( typeof define !== "function" ) {
-define = require( "amdefine" )( module )
-}
-define(function() {
-var tmpl=tmpl|| {};
- tmpl['tmpl']=function anonymous(it) {
-var out='<li class="something"><a>'+(it.something)+'</a></li>';return out;
-}
-return tmpl;});
