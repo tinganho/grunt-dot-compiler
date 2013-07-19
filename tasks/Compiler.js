19fb8a5417e52d7742e63a1c8e8005942805c3ee
Tingan Ho <tingan87@gmail.com>
2013-07-19 15:37:32 +0800

    Finished sonic templates

---
+++ tasks/Compiler.js
@@ -0,0 +1,183 @@
+
+/**
+ * Module dependencies.
+ */
+
+var grunt = require('grunt')
+  , _     = grunt.util._
+  , path  = require('path')
+  , fs    = require('fs')
+  , doT   = require('dot')
+
+/**
+ * Compiler
+ */
+
+var Compiler = function(opt) {
+  this.opt = _.defaults(opt || {}, {
+    variable  : 'tmpl',
+    node      : false,
+    root      : opt.gruntRoot,
+    requirejs : false,
+    key       : function(filepath) {
+      return path.basename(filepath, path.extname(filepath));
+    }
+  });
+  this.opt.variable = opt.variable.replace('window.', '');
+  if(this.opt.root.substr(-1) !== '/') {
+    this.opt.root += '/';
+  }
+
+  this.loadRegex = /\{\{\#\#\s*(def\.\w+)\s*\:\s*load\(['|"](.*?)['|"]\,?\s*(\{\s*(.*?\s*?)+?\})?\s*\);?\s*\#?\}\}/g;
+ 
+  _.bindAll(this);
+};
+
+/**
+ * Get absolute path.
+ */
+
+Compiler.prototype.getAbsolutePath = function(filePath, loadPath) {
+  var res;
+  // Check relative path
+  if(/^\./.test(loadPath)) {
+    res = path.join(this.opt.gruntRoot, path.dirname(filePath), loadPath);
+  } else {
+    res = path.join(this.opt.root, loadPath);
+  }
+  return res;
+};
+
+/**
+ * Load partials
+ */
+
+Compiler.prototype.loadPartials = function(m, filePath, loadPath, obj) {
+  var customVars = {}, _this = this, pendingPartialLoads = {};
+
+  if(typeof obj !== 'undefined') {
+    var matches = obj.match(/(\w+)\s*\:(.*)\s*/g);
+    for(var i = 0; i < matches.length; i++) {
+      var _matches = /(\w+)\s*\:(.*)\s*/g.exec(matches[i]);
+      customVars[_matches[1]] = _matches[2].replace(/'|"|\,|\s*/g, '');
+    }
+  }
+
+  var content = fs.readFileSync(this.getAbsolutePath(filePath, loadPath), 'utf8');
+
+  if(this.loadRegex.test(content)) {
+    content = content.replace(this.loadRegex, function(m, namespace, loadPath, obj) {
+      var content = _this.loadPartials(m, filePath, loadPath, obj);
+      pendingPartialLoads[namespace] = content;
+      return '';
+    });
+  }
+
+  this.loadPendingPartials(content, pendingPartialLoads);
+
+  if(typeof obj !== 'undefined') {
+    for(var key in customVars) {
+      var regex = new RegExp('\\{\\{\\$\\s*(' + key + ')\\s*\\:?\\s*(.*?)\\s*\\}\\}');
+      content = content.replace(regex, function(m, key, defaultValue) {
+        if(typeof customVars[key] === 'undefined' && typeof defaultValue === 'undefined') {
+          return '';
+        } else if(typeof val !== 'undefined') {
+          return defaultValue;
+        } else {
+          return customVars[key];
+        }
+      });
+    }
+  }
+
+  return content;
+};
+
+Compiler.prototype.loadPendingPartials = function(content, pendingPartialLoads) {
+  for(var namespace in pendingPartialLoads) {
+    content = content.replace(
+      new RegExp('\\{\\{\\#\\s*' + namespace + '\\s*\\}\\}'), 
+      function(m) {
+        return pendingPartialLoads[namespace];
+      });
+  }
+  return content;
+};
+
+Compiler.prototype.getFileContent = function(filePath) {
+  var _this = this, pendingPartialLoads = {};
+
+  var content = grunt.file.read(filePath)
+    .replace(/\/\/.*\n/g,'')
+    .replace(this.loadRegex, function(m, namespace, loadPath, obj) {
+      var content = _this.loadPartials(m, filePath, loadPath, obj);
+      pendingPartialLoads[namespace] = content;
+      return '';
+    })
+    .replace(/\{\{\$\s*\w*?\s*\:\s*(.*?)\s*\}\}/g, function(m, p1) {
+      return p1;
+    })
+    .replace(/^\s+|\s+$|[\r\n]+/gm, '')
+    .replace(/\/\*.*?\*\//gm,'');
+  this.loadPendingPartials(content, pendingPartialLoads);
+  return content;
+};
+
+Compiler.prototype.compileTemplates = function(files) {
+
+  var js = '', _this = this;
+
+  // RequireJS
+  if(!this.opt.requirejs && !this.opt.node) {
+    if(this.opt.variable.indexOf('.') !== -1) {
+      js += this.opt.variable + ' = (function(){' + grunt.util.linefeed;
+    } else {
+      js += 'var ' + this.opt.variable + ' = (function(){' + grunt.util.linefeed;
+    }
+  }
+
+  if(this.opt.requirejs && this.opt.node) {
+    js += 'if(typeof define !== \'function\') {' + grunt.util.linefeed;
+    js += '  define = require(\'amdefine\')(module);' + grunt.util.linefeed;
+    js += '}' + grunt.util.linefeed;
+  }
+
+  if(this.opt.requirejs) {
+    js += 'define(function() {' + grunt.util.linefeed;
+  }
+
+  // Defining encodeHTML method for the templates
+  js += 'function encodeHTMLSource() {';
+  js += '  var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", \'"\': \'&#34;\', "\'": \'&#39;\', "/": \'&#47;\' },';
+  js += '  matchHTML = /&(?!#?\w+;)|<|>|"|\'|\\//g;';
+  js += '  return function() {';
+  js += '    return this ? this.replace(matchHTML, function(m) {return encodeHTMLRules[m] || m;}) : this;';
+  js += '  };';
+  js += '};' + grunt.util.linefeed;
+  js += 'String.prototype.encodeHTML=encodeHTMLSource();' + grunt.util.linefeed;
+
+  js += 'var tmpl = {};' + grunt.util.linefeed;
+
+  files.map(function(filePath) {
+    var template = _this.getFileContent(filePath)
+      , fn       = doT.template(template)
+      , key      = _this.opt.key(filePath);
+    js += '  tmpl' + "['" + key + "']=" + fn + ';' + grunt.util.linefeed;
+  });
+
+
+  if(!this.opt.requirejs && !this.opt.node) {
+    js += 'return tmpl;})()';
+  } else if(this.opt.requirejs) {
+    js += 'return tmpl;});' + grunt.util.linefeed;
+  } else if(this.opt.simple && this.opt.node){
+    js += '';
+  } else if(this.opt.node) {
+    js += 'module.exports = tmpl;';
+  }
+
+  return js;
+
+};
+
+module.exports = Compiler;
---
+++ tasks/dot.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies.
+ */
+
+var path = require('path')
+  , Compiler = require('./Compiler');
+
+/*
+ * Export grunt task
+ */
+
+ module.exports = function(grunt) {
+
+  "use strict";
+
+  grunt.registerMultiTask('dot', 'prepares and combines any type of template into a script include', function() {
+    this.data.options.gruntRoot = path.dirname(grunt.file.findup('Gruntfile.js')) + '/';
+    var files = grunt.file.expand({filter: 'isFile'}, this.files[0].src)
+      , compiler = new Compiler(this.data.options)
+      , src = compiler.compileTemplates(files);
+    grunt.file.write(this.files[0].dest, src);
+    grunt.log.writeln('File "' + this.files[0].dest + '" created.');
+  });
+};
