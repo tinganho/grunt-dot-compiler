14d693bf88fdb153972e204ae018a2bcd54e5712
Gilad Peleg <giladp007@gmail.com>
2013-12-30 15:32:31 +0200

    update task to deal with new grunt mappings

--- tasks/dot.js
+++ tasks/dot.js
@@ -1,26 +1,34 @@
-
 /**
  * Module dependencies.
  */
 
-var path = require('path')
-  , Compiler = require('../src/Compiler');
+var path = require('path'),
+    Compiler = require('../src/Compiler');
 
 /*
  * Export grunt task
  */
 
- module.exports = function(grunt) {
+module.exports = function (grunt) {
+    'use strict';
 
-  "use strict";
+    grunt.registerMultiTask('dot', 'prepares and combines any type of template into a script include', function () {
+        var options = this.options();
+        options.gruntRoot = path.dirname(grunt.file.findup('Gruntfile.js')) + '/';
 
-  grunt.registerMultiTask('dot', 'prepares and combines any type of template into a script include', function() {
-    this.data.options.gruntRoot = path.dirname(grunt.file.findup('Gruntfile.js')) + '/';
-    var files = grunt.file.expand({ filter: 'isFile' }, this.files[0].src)
-      , compiler = new Compiler(this.data.options)
-      , src = compiler.compileTemplates(files);
+        this.files.forEach(function (file) {
+            var compiler, src, existingSrcFiles;
 
-    grunt.file.write(this.files[0].dest, src);
-    grunt.log.writeln('File "' + this.files[0].dest + '" created.');
-  });
+            existingSrcFiles = file.src.filter(function (_file) {
+                return grunt.file.exists(_file);
+            });
+
+            //Write joined contents to destination filepath
+            compiler = new Compiler(options);
+            src = compiler.compileTemplates(existingSrcFiles);
+
+            grunt.file.write(file.dest, src);
+            grunt.log.writeln('File "' + file.dest + '" created.');
+        });
+    });
 };
