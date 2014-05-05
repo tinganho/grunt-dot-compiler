4d072e4ca7acb2cb91f6df4d42d617ab9631eb9a
arusanov <alexander.rusanov@gmail.com>
2014-05-06 01:00:39 +0800

    added singleExport option

--- src/Compiler.js
+++ src/Compiler.js
@@ -3,30 +3,30 @@
  */
 
 var grunt = require('grunt')
-  , _     = grunt.util._
-  , path  = require('path')
-  , fs    = require('fs')
-  , doT   = require('dot')
+  , _ = grunt.util._
+  , path = require('path')
+  , fs = require('fs')
+  , doT = require('dot')
 
 /**
  * Compiler.
  */
 
-var Compiler = function(opt) {
+var Compiler = function (opt) {
   this.opt = _.defaults(opt || {}, {
-    variable  : 'tmpl',
-    node      : false,
-    root      : opt.gruntRoot,
-    requirejs : false,
+    variable: 'tmpl',
+    node: false,
+    root: opt.gruntRoot,
+    requirejs: false,
     selfcontained: true,
-    key       : function(filepath) {
+    key: function (filepath) {
       return path.basename(filepath, path.extname(filepath));
-    }, dotJS:{
+    }, dotJS: {
 
     }
   });
   this.opt.variable = opt.variable.replace('window.', '');
-  if(this.opt.root.substr(-1) !== '/') {
+  if (this.opt.root.substr(-1) !== '/') {
     this.opt.root += '/';
   }
 
@@ -39,10 +39,10 @@ var Compiler = function(opt) {
  * Get absolute path.
  */
 
-Compiler.prototype.getAbsolutePath = function(filePath, loadPath) {
+Compiler.prototype.getAbsolutePath = function (filePath, loadPath) {
   var res;
   // Check relative path
-  if(/^\./.test(loadPath)) {
+  if (/^\./.test(loadPath)) {
     res = path.join(this.opt.gruntRoot, path.dirname(filePath), loadPath);
   }
   else {
@@ -55,38 +55,38 @@ Compiler.prototype.getAbsolutePath = function(filePath, loadPath) {
  * Load partials.
  */
 
-Compiler.prototype.loadPartial = function(m, filePath, loadPath, obj) {
+Compiler.prototype.loadPartial = function (m, filePath, loadPath, obj) {
   var customVars = {}, _this = this, pendingPartialLoads = {};
 
   var _filePath = this.getAbsolutePath(filePath, loadPath);
   var content = fs.readFileSync(_filePath, 'utf8').replace(/<\!\-\-(.|\n)*\!\-\->/g, '');
 
-  if(this.loadRegex.test(content)) {
+  if (this.loadRegex.test(content)) {
     _filePath = _filePath.replace(_this.opt.gruntRoot, '');
-    content = content.replace(this.loadRegex, function(m, namespace, loadPath, obj) {
+    content = content.replace(this.loadRegex, function (m, namespace, loadPath, obj) {
       var content = _this.loadPartial(m, _filePath, loadPath, obj);
       pendingPartialLoads[namespace] = content;
       return '';
     });
   }
 
-  if(typeof obj !== 'undefined') {
+  if (typeof obj !== 'undefined') {
     var matches = obj.match(/(\w+)\s*\:(.*)\s*/g);
-    for(var i = 0; i < matches.length; i++) {
+    for (var i = 0; i < matches.length; i++) {
       var _matches = /(\w+)\s*\:(.*)\s*/g.exec(matches[i])
         , key = _matches[1]
         , value = _matches[2].replace(/'|"|\,|\s*/g, '')
         , regex = new RegExp('\\{\\{\\$\\s*(' + key + ')\\s*\\:?\\s*(.*?)\\s*\\}\\}', 'g');
 
-        content = content.replace(regex, function(m, key, defaultValue) {
-          if(typeof value === 'undefined' && typeof defaultValue === 'undefined') {
-            return '';
-          } else if(typeof val !== 'undefined') {
-            return defaultValue;
-          } else {
-            return value;
-          }
-        });
+      content = content.replace(regex, function (m, key, defaultValue) {
+        if (typeof value === 'undefined' && typeof defaultValue === 'undefined') {
+          return '';
+        } else if (typeof val !== 'undefined') {
+          return defaultValue;
+        } else {
+          return value;
+        }
+      });
     }
   }
 
@@ -96,12 +96,12 @@ Compiler.prototype.loadPartial = function(m, filePath, loadPath, obj) {
   return content;
 };
 
-Compiler.prototype.setDefaultValues = function(content) {
+Compiler.prototype.setDefaultValues = function (content) {
   content = content
-    .replace(/\{\{\$\s*\w*?\s*\:\s*(.*?)\s*\}\}/g, function(m, p1) {
+    .replace(/\{\{\$\s*\w*?\s*\:\s*(.*?)\s*\}\}/g, function (m, p1) {
       return p1;
     })
-    .replace(/\{\{\$(.*?)\}\}/g, function() {
+    .replace(/\{\{\$(.*?)\}\}/g, function () {
       return '';
     });
   return content;
@@ -112,11 +112,11 @@ Compiler.prototype.setDefaultValues = function(content) {
  * Loads pending partials.
  */
 
-Compiler.prototype.loadPendingPartials = function(content, pendingPartialLoads) {
-  for(var namespace in pendingPartialLoads) {
+Compiler.prototype.loadPendingPartials = function (content, pendingPartialLoads) {
+  for (var namespace in pendingPartialLoads) {
     content = content.replace(
       new RegExp('\\{\\{\\#\\s*' + namespace + '\\s*\\}\\}', 'g'),
-      function(m) {
+      function (m) {
         return pendingPartialLoads[namespace];
       });
   }
@@ -127,7 +127,7 @@ Compiler.prototype.loadPendingPartials = function(content, pendingPartialLoads) 
  * Get file content.
  */
 
-Compiler.prototype.getFileContent = function(filePath) {
+Compiler.prototype.getFileContent = function (filePath) {
   var _this = this, pendingPartialLoads = {};
 
   // Log file path
@@ -136,14 +136,14 @@ Compiler.prototype.getFileContent = function(filePath) {
   // Return file content
   var content = grunt.file.read(filePath)
     .replace(/^(?!.*:\/\/$).*\/\/.*/, '')
-    .replace(this.loadRegex, function(m, namespace, loadPath, obj) {
+    .replace(this.loadRegex, function (m, namespace, loadPath, obj) {
       var content = _this.loadPartial(m, filePath, loadPath, obj);
       pendingPartialLoads[namespace] = content;
       return '';
     })
     .replace(/<\!\-\-(.|\n)*\!\-\->/g, '')
     .replace(/^\s+|\s+$|[\r\n]+/gm, '')
-    .replace(/\/\*.*?\*\//gm,'');
+    .replace(/\/\*.*?\*\//gm, '');
 
   content = this.loadPendingPartials(content, pendingPartialLoads);
   return content;
@@ -153,27 +153,27 @@ Compiler.prototype.getFileContent = function(filePath) {
  * Compile templates.
  */
 
-Compiler.prototype.compileTemplates = function(files) {
+Compiler.prototype.compileTemplates = function (files) {
 
   var js = '', _this = this;
-  
+
 
   // RequireJS
-  if(!this.opt.requirejs && !this.opt.node) {
-    if(this.opt.variable.indexOf('.') !== -1) {
+  if (!this.opt.requirejs && !this.opt.node) {
+    if (this.opt.variable.indexOf('.') !== -1) {
       js += this.opt.variable + ' = (function(){' + grunt.util.linefeed;
     } else {
       js += 'var ' + this.opt.variable + ' = (function(){' + grunt.util.linefeed;
     }
   }
 
-  if(this.opt.requirejs && !this.opt.node) {
+  if (this.opt.requirejs && !this.opt.node) {
     js += 'if(typeof define !== \'function\') {' + grunt.util.linefeed;
     js += '  define = require(\'amdefine\')(module);' + grunt.util.linefeed;
     js += '}' + grunt.util.linefeed;
   }
 
-  if(this.opt.requirejs) {
+  if (this.opt.requirejs) {
     js += 'define(function() {' + grunt.util.linefeed;
   }
 
@@ -188,23 +188,28 @@ Compiler.prototype.compileTemplates = function(files) {
     js += '};' + grunt.util.linefeed;
     js += 'String.prototype.encodeHTML=encodeHTMLSource();' + grunt.util.linefeed;
   }
-
-  js += 'var tmpl = {};' + grunt.util.linefeed;
-
-  files.map(function(filePath) {
+  if (!this.opt.singleExport) {
+    js += 'var tmpl = {};' + grunt.util.linefeed;
+  }
+  files.map(function (filePath) {
     var template = _this.getFileContent(filePath)
-      , fn       = doT.template(template, _.defaults(_this.opt.dotJS || {}, doT.templateSettings) )
-      , key      = _this.opt.key(filePath);
-    js += '  tmpl' + "['" + key + "']=" + fn + ';' + grunt.util.linefeed;
+      , fn = doT.template(template, _.defaults(_this.opt.dotJS || {}, doT.templateSettings))
+      , key = _this.opt.key(filePath);
+
+    if (!this.opt.singleExport) {
+      js += '  tmpl' + "['" + key + "']=" + fn + ';' + grunt.util.linefeed;
+    } else {
+      js += 'var tmpl = ' + fn + ';' + grunt.util.linefeed;
+    }
   });
 
-  if(!this.opt.requirejs && !this.opt.node) {
+  if (!this.opt.requirejs && !this.opt.node) {
     js += 'return tmpl;})();';
-  } else if(this.opt.requirejs) {
+  } else if (this.opt.requirejs) {
     js += 'return tmpl;});' + grunt.util.linefeed;
-  } else if(this.opt.simple && this.opt.node){
+  } else if (this.opt.simple && this.opt.node) {
     js += '';
-  } else if(this.opt.node) {
+  } else if (this.opt.node) {
     js += 'module.exports = tmpl;';
   }
 
